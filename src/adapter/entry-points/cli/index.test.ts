a63008270925bb7802ae5138f9d31372b2e26f42
HiromiShikata <shikata.hiromi@gmail.com>
2024-09-16 07:15:01 +0000

    feat: implement cli interface

--- src/adapter/entry-points/cli/index.test.ts
+++ src/adapter/entry-points/cli/index.test.ts
@@ -1,28 +1,53 @@
 import { execSync } from 'child_process';
+import { WebClient } from '@slack/web-api';
+import { config } from 'dotenv';
+
+config();
 
 describe('commander program', () => {
+  jest.setTimeout(120000);
   it('should output help contents', () => {
     const output = execSync(
       'npx ts-node ./src/adapter/entry-points/cli/index.ts -h',
     ).toString();
 
-    expect(output.trim()).toEqual(`Usage: Example CLI [options] <path>
+    expect(output.trim()).toEqual(`Usage: slack-message-deleter [options]
 
-This is an example
-
-Arguments:
-  path        Path of example
+Clean up old messages in slack channel
 
 Options:
-  -h, --help  display help for command`);
+  -t, --token <string>    SlackBotToken
+  -c, --channel <string>  ChannelId
+  -m, --minutes <number>  DeleteMessagesOlderThanThisNumberOfMinutes
+  -h, --help              display help for command`);
   });
-  it('should output file contents', () => {
+  it('should delete message', async () => {
+    const token = process.env.SLACK_BOT_TOKEN;
+    if (!token) {
+      throw new Error('SLACK_BOT_TOKEN is not set');
+    }
+    const channel = process.env.SLACK_CHANNEL_ID || 'CFNN90G07';
+    const minutes = 1;
+    await postMessage(token, channel, 'test');
+    await new Promise((resolve) => setTimeout(resolve, 60000));
+
     const output = execSync(
-      'npx ts-node ./src/adapter/entry-points/cli/index.ts ./testdata/src/domain/entities',
+      `npx ts-node ./src/adapter/entry-points/cli/index.ts -t ${token} -c ${channel} -m ${minutes}`,
     ).toString();
 
-    expect(output.trim()).toEqual(
-      JSON.stringify('./testdata/src/domain/entities'),
-    );
+    expect(output.trim().startsWith('Deleted ')).toEqual(true);
   });
 });
+const postMessage = async (token: string, channel: string, text: string) => {
+  const web = new WebClient(token);
+  await web.conversations.join({ channel });
+
+  const result = await web.chat.postMessage({
+    channel,
+    text,
+  });
+  if (!result.ok) {
+    throw new Error('Failed to post message');
+  }
+  return result;
+};
